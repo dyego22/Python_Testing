5dcd0b4c27bef59bdc1e948257ee0c5920728161
dyego22 <118778549+dyego22@users.noreply.github.com>
2023-04-20 08:58:51 -0300

    Avanzados clase 2

---
+++ SJTEC/main.py
@@ -0,0 +1,5 @@
+import matematica as mat
+#Configurando el archivo __init_.py del paquete matematica puedo llamar de una manera mas limpias a los modulos
+
+print(mat.suma(3,4))
+print(mat.potencia(2,3))
---
+++ SJTEC/matematica/__init__.py
@@ -0,0 +1,2 @@
+from .elementales import suma
+from .nivel2 import potencia
